53392c3b7e63aa65feac9290e41dd2abd1e28242
Rafael Mercado <A01746991@itesm.mx>
2024-08-27 18:56:45 -0600

    Servidor pytho-flask

---
+++ serverML.py
@@ -0,0 +1,25 @@
+#Python Libraries 
+from flask import Flask, request, jsonify, render_template
+import numpy as np
+from load import joblib
+import os 
+from werkzeug.utils import secure_filename
+
+#Load model
+dt = joblib.load('dt1_ml.joblib')
+#Create Flask App
+server = Flask(__name__)
+#Define route
+@server.route('/predictjson', methods=['POST'])
+def predictjson():
+    data= request.json
+    print(data)
+    inputData= np.array([
+        data["pH"],
+        data["sulphates"],
+        data["alcohol"]
+    ])
+    result= dt.predict(inputData.reshape(1,-1))
+    return jsonify({"Prediction": str(result[0])})
+if __name__ == "__main__":
+    server.run(debug= False , host ="0.0.0.0",port=8080)
